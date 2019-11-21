ab59dfb9704aa6e9d390b15216afbd9cc03d64e7
nishkalaj <56379460+nishkalaj@users.noreply.github.com>
2019-11-21 11:58:23 +0530

    Update App.java

--- junit/src/main/java/com/github/junitpack/App.java
+++ junit/src/main/java/com/github/junitpack/App.java
@@ -5,7 +5,9 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(new App().greet("world"));
-
+if(true){
+	
+}
 	}
 
 	public String greet(String name){
