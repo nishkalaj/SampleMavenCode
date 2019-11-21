0df7c28290835f8183d63867439a52218f13970d
nishkalaj <56379460+nishkalaj@users.noreply.github.com>
2019-11-21 23:17:32 +0530

    Update App.java
    Removing the extra Code

--- junit/src/main/java/com/github/junitpack/App.java
+++ junit/src/main/java/com/github/junitpack/App.java
@@ -5,9 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(new App().greet("world"));
-if(true){
-	
-}
+
 	}
 
 	public String greet(String name){
